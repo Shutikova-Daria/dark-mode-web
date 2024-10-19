0d3a18056132da941b6ab187e47af647526e3afc
Shutikova-Daria <shutikovad224@gmail.com>
2024-10-19 13:48:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function clickDark() {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+}
+
+function clickLight() {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+}
+
+
